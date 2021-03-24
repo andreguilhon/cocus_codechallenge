6b5c4a6f920dffa2199a4fbba21cb653eb31e793
André Guilhon <andre.guilhon@portergroup.com.br>
2021-03-24 17:28:07 -0300

    Code cleanup

--- filereader/reader/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import FileContent
-
-
-class FileContentSerializer(serializers.Serializer):
-    line_number = serializers.IntegerField()
-    line_content = serializers.CharField()
-    frequent_char = serializers.CharField()
